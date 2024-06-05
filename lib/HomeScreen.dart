2475e3fdcb8ca52f3b87258a43dff8feb8b73d84
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-06-05 16:57:05 +0500

    1.Practice in Getx

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -21,23 +21,54 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
              Card(
                child: ListTile(
-                 title:const  Text('Getx Dialog Alert'),
-                 subtitle:const  Text('Practicing Dialog Alert'),
+                 title:const  Text('Getx Alert Dialog'),
+                 subtitle:const  Text('Practicing Alert  Dialog'),
                  onTap: (){
-                   Get.defaultDialog(
-                     title: 'Delete Chat',
-                     middleText: 'Are You Sure you want to Delete chat',
-                     contentPadding:const  EdgeInsets.all(20),
-                     titlePadding:const EdgeInsets.only(top: 20),
-                     confirm: TextButton(onPressed: (){
-                       Navigator.pop(context);
-                     }, child:const  Text('Yes')),
-                     cancel: TextButton(onPressed: (){}, child:const  Text('No'))
-
-                   );
+                  Get.defaultDialog(
+                    title: 'Delete chat',
+                    middleText: 'Are You sure you want to Delete',
+                    confirm: TextButton(onPressed: (){
+                      Navigator.pop(context);
+                    }, child:const  Text('Yes')),
+                    cancel: TextButton(onPressed: (){}, child:const  Text('NO'))
+                  );
                  },
                ),
-             )
+             ),
+          Card(
+            child: ListTile(
+              title:const  Text('Getx Bottom Sheet'),
+              subtitle:const  Text('Practicing Bottom sheet'),
+              onTap: (){
+                Get.bottomSheet(
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              leading:const  Icon(Icons.light_mode),
+                              title:const  Text('Light Mode'),
+                              onTap: (){
+                                Get.changeTheme(ThemeData.light());
+                              },
+                            ),
+                            ListTile(
+                              leading: Icon(Icons.dark_mode),
+                              title: Text('Light Mode'),
+                              onTap: (){
+                                Get.changeTheme(ThemeData.dark());
+                              },
+                            )
+                          ],
+                        )
+                    )
+                );
+              },
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(onPressed:(){
